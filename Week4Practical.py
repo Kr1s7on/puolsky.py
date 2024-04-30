47346faed8060a779cf33f6204bb6e2e50627337
Kriston Jomari <231165r@mymail.nyp.edu.sg>
2024-04-30 16:48:13 +0800

    started on week 4, done till slide 14 only

---
+++ Week4Practical.py
@@ -0,0 +1,12 @@
+value1 = 5
+value1 = 10
+value2 = value1
+print (value1)
+# It will print the one nearer to it, so in this case it's 10
+print ('value1=', value1)
+print ('value2=', value2)
+
+print('id of value1=', id(value1))
+print('id of value2=', id(value2))
+print('type of value1=', type(value1))
+print('type of value2=', type(value2))
