d2182fa36f2e64ce4a73e082980c323bc590dcf1
Kriston Jomari <231165r@mymail.nyp.edu.sg>
2024-05-06 14:32:54 +0800

    Week 4 stuff

---
+++ RectangleArea.py
@@ -0,0 +1,11 @@
+# Defining a class with a method that takes parameters
+length = int(input("Enter the length of the rectangle: "))
+
+# Defining a class with a method that takes parameters
+breadth = int(input("Enter the breadth of the rectangle: "))
+
+# Calculating the area of the rectangle
+area = length * breadth
+
+# Displaying the area of the rectangle
+print ("The area of the rectangle is", str(area)+' meters squared')
