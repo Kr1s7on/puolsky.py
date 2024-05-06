6395cf3a6f496008d07f95fe0003a677655c40c5
Kriston Jomari <231165r@mymail.nyp.edu.sg>
2024-05-06 14:43:35 +0800

    edits

--- RectangleArea.py
+++ RectangleArea.py
@@ -1,11 +1,11 @@
 # Defining a class with a method that takes parameters
-length = int(input("Enter the length of the rectangle: "))
+length = float(input("Enter the length of the rectangle: "))
 
 # Defining a class with a method that takes parameters
-breadth = int(input("Enter the breadth of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
 
 # Calculating the area of the rectangle
 area = length * breadth
 
 # Displaying the area of the rectangle
-print ("The area of the rectangle is", str(area)+' meters squared!')
+print ("The area of the rectangle whose length is %.2f, width is %.2f is %.2f meters suared!" % (length, breadth, area))
