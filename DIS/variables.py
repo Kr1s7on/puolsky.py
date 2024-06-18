4646fc7045413e209800ad9fb1c9736f63cde22a
Kriston Jomari <231165r@mymail.nyp.edu.sg>
2024-06-18 14:51:51 +0800

    rm DIS/variables.py

--- DIS/variables.py
+++
@@ -1,9 +0,0 @@
-# Exercise
-# Write a program that asks a user for their name, age, and favorite color, and then prints them.
-
-name = input("Please enter your name: ")
-age = input("Please enter your age: ")
-FavColor = input("Please enter your favorite color: ")
-
-print("Your name is " + name + ", you are " + age + " years old, and your favorite color is " + FavColor + ".")
-
